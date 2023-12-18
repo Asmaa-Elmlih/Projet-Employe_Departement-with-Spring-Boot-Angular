d8cb6980a6cf619bcdee214a88ba00fd07b26916
Asmaa Elmlih <elmlihasmae@gmail.com>
2023-12-18 01:37:53 +0100

    Supprimer tout le contenu de la branche backend2

--- src/main/java/com/example/EmployeDepartement/repositories/DepartementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EmployeDepartement.repositories;
-
-import com.example.EmployeDepartement.models.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartementRepository extends JpaRepository<Department,Long> {
-}
